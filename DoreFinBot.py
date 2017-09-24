43b9782857bddc0fd15152eb64e761632a9e0e9b
DanielDDore <danielddore@gmail.com>
2017-09-23 20:43:28 -0500

    Added algorithms for investing in stocks

--- DoreFinBot.py
+++ DoreFinBot.py
@@ -31,51 +31,51 @@ def main():
     #Default price is 1000, so try to make some profit on bonds:
     #990 is buy price and 1002 is sell price.
 
-    write(exchange, {"type": "add", "order_id": 1, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 20})
+    write(exchange, {"type": "add", "order_id": 1, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 20})
 
     write(exchange, {"type": "add", "order_id": 2, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 20})
 
-    write(exchange, {"type": "add", "order_id": 3, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 40})
+    write(exchange, {"type": "add", "order_id": 3, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 40})
 
     write(exchange, {"type": "add", "order_id": 4, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 40})
 
-    write(exchange, {"type": "add", "order_id": 5, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 20})
+    write(exchange, {"type": "add", "order_id": 5, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 20})
 
     write(exchange, {"type": "add", "order_id": 6, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 20})
 
-    write(exchange, {"type": "add", "order_id": 7, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 30})
+    write(exchange, {"type": "add", "order_id": 7, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 30})
 
     write(exchange, {"type": "add", "order_id": 8, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 30})
 
-    write(exchange, {"type": "add", "order_id": 9, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 40})
+    write(exchange, {"type": "add", "order_id": 9, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 40})
 
     write(exchange, {"type": "add", "order_id": 10, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 40})
 
-    write(exchange, {"type": "add", "order_id": 11, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 10})
+    write(exchange, {"type": "add", "order_id": 11, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 10})
 
     write(exchange, {"type": "add", "order_id": 12, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 10})
 
-    write(exchange, {"type": "add", "order_id": 13, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 30})
+    write(exchange, {"type": "add", "order_id": 13, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 30})
 
     write(exchange, {"type": "add", "order_id": 14, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 30})
 
-    write(exchange, {"type": "add", "order_id": 15, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 40})
+    write(exchange, {"type": "add", "order_id": 15, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 40})
 
     write(exchange, {"type": "add", "order_id": 16, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 40})
 
-    write(exchange, {"type": "add", "order_id": 17, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 40})
+    write(exchange, {"type": "add", "order_id": 17, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 40})
 
     write(exchange, {"type": "add", "order_id": 18, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 40})
 
-    write(exchange, {"type": "add", "order_id": 19, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 30})
+    write(exchange, {"type": "add", "order_id": 19, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 30})
 
     write(exchange, {"type": "add", "order_id": 20, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 30})
 
-    write(exchange, {"type": "add", "order_id": 21, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 20})
+    write(exchange, {"type": "add", "order_id": 21, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 20})
 
     write(exchange, {"type": "add", "order_id": 22, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 20})
 
-    write(exchange, {"type": "add", "order_id": 23, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 10})
+    write(exchange, {"type": "add", "order_id": 23, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 10})
 
     write(exchange, {"type": "add", "order_id": 24, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 10})
 
