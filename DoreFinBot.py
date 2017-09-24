513cace581c6cf9584f61c3af08740ca16ab7e66
DanielDDore <danielddore@gmail.com>
2017-09-23 21:42:47 -0500

    Finalizing bond trading bot for ETC at UIUC. Came in ninth place with 100k score.

--- DoreFinBot.py
+++ DoreFinBot.py
@@ -29,64 +29,15 @@ def main():
     #Algorithm for Bonds:
     #Bonds are the safest, so buy low and sell high:
     #Default price is 1000, so try to make some profit on bonds:
-    #990 is buy price and 1002 is sell price.
+    #After doing risk analysis of different derivatives,
+    #Decided bonds was safest method
+    #Got P/L Up to 300 in rounds
+    #Finished in the top 10 with 100k score
 
-    write(exchange, {"type": "add", "order_id": 1, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 20})
-
-    write(exchange, {"type": "add", "order_id": 2, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 20})
-
-    write(exchange, {"type": "add", "order_id": 3, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 40})
-
-    write(exchange, {"type": "add", "order_id": 4, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 40})
-
-    write(exchange, {"type": "add", "order_id": 5, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 40})
-
-    write(exchange, {"type": "add", "order_id": 6, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 40})
-
-    write(exchange, {"type": "add", "order_id": 7, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 30})
-
-    write(exchange, {"type": "add", "order_id": 8, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 30})
-
-    write(exchange, {"type": "add", "order_id": 9, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 40})
-
-    write(exchange, {"type": "add", "order_id": 10, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 40})
-
-    write(exchange, {"type": "add", "order_id": 11, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 20})
-
-    write(exchange, {"type": "add", "order_id": 12, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 20})
-
-    write(exchange, {"type": "add", "order_id": 13, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 30})
-
-    write(exchange, {"type": "add", "order_id": 14, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 30})
-
-    write(exchange, {"type": "add", "order_id": 15, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 40})
-
-    write(exchange, {"type": "add", "order_id": 16, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 40})
-
-    write(exchange, {"type": "add", "order_id": 17, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 10})
-
-    write(exchange, {"type": "add", "order_id": 18, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 10})
-
-    write(exchange, {"type": "add", "order_id": 19, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 10})
-
-    write(exchange, {"type": "add", "order_id": 20, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 10})
-
-    write(exchange, {"type": "add", "order_id": 21, "symbol": "BOND", "dir": "BUY", "price": 999, "size": 10})
-
-    write(exchange, {"type": "add", "order_id": 22, "symbol": "BOND", "dir": "SELL", "price": 1001, "size": 10})
-
-    write(exchange, {"type": "add", "order_id": 23, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 50})
-
-    write(exchange, {"type": "add", "order_id": 24, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 50})
-
-
-
-
-
-
-    #Trading for stocks:
-    #Goldman Sachs keeps falling so do not buy
-    #Morgan Stanley is staying steadily, so I'll invest:
+    i = 0
+    for (i in range(0, 20)):
+        write(exchange, {"type": "add", "order_id": i++, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 40})
+        write(exchange, {"type": "add", "order_id": i++, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 40})
 
     #Exchange runs:
     print("The exchange replied:", hello_from_exchange, file=sys.stderr)
