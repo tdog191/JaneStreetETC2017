4db846016000b57334361bf3e7dcc72a8e159e26
DanielDDore <danielddore@gmail.com>
2017-09-23 19:59:14 -0500

    Added algorithms for investing in stocks

--- DoreFinBot.py
+++ DoreFinBot.py
@@ -29,21 +29,21 @@ def main():
     #Algorithm for Bonds:
     #Bonds are the safest, so buy low and sell high:
     #Default price is 1000, so try to make some profit on bonds:
-    #998 is buy price and 1002 is sell price.
+    #990 is buy price and 1002 is sell price.
 
     amountOfBondsPurchased = 5
 
     bondAddID = 0
 
     for bondAddID in range(0,5):
-        write(exchange, {"type": "add", "order_id": bondAddID, "symbol": "BOND", "dir": "BUY", "price": 998, "size": 5})
+        write(exchange, {"type": "add", "order_id": bondAddID, "symbol": "BOND", "dir": "BUY", "price": 990, "size": 5})
 
     amountOfBoundsSold = 6
 
     bondSellID = 1
 
     for boundSellID in range(1,6):
-        write(exchange, {"type": "add", "order_id": bondSellID, "symbol": "BOND", "dir": "SELL", "price": 1002, "size": 5})
+        write(exchange, {"type": "add", "order_id": bondSellID, "symbol": "BOND", "dir": "SELL", "price": 1010, "size": 5})
 
     #Trading for stocks:
     #Goldman Sachs keeps falling so do not buy
@@ -52,8 +52,8 @@ def main():
     MSID = 7
 
     for MSID in range(7, 12):
-        write(exchange, {"type": "add", "order_id": MSID, "symbol": "MS", "dir": "BUY", "price": 3995 , "size": 1})
-        write(exchange, {"type": "add", "order_id": MSID, "symbol": "MS", "dir": "BUY", "price": 4005 , "size": 1})
+        write(exchange, {"type": "add", "order_id": MSID, "symbol": "MS", "dir": "BUY", "price": 3500 , "size": 1})
+        write(exchange, {"type": "add", "order_id": MSID, "symbol": "MS", "dir": "BUY", "price": 4500 , "size": 1})
 
     #Exchange runs:
     print("The exchange replied:", hello_from_exchange, file=sys.stderr)
